bbf79555c92df734d4bbc2b0675a7d505ea14355
Soujanya <01fe22bca097@kletech.ac.in>
2024-03-16 23:27:09 +0530

    Student Course  Details

---
+++ Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+    public void stud_det() {
+        String name = "Soujanya";
+        String prg = "BCA";
+        String dept = "MCA";
+        String[] course = { "DevOps", "Machine Learning", "C#" };
+        System.out.println("Name of the Student is = " + name);
+        System.out.println("Program of the student is = " + prg);
+        System.out.println("Department of the student is = " + dept);
+        for (int i = 0; i < course.length; i++) {
+
+            System.out.println(course[i]);
+        }
+
+    }
+}
